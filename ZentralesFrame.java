adf4f11ab7bc67e776fe6e463bc6a275e0ee0a8e
mk-2025-spec <maximilian.krueper@web.de>
2026-06-15 08:01:24 +0200

    gg

--- ZentralesFrame.java
+++ ZentralesFrame.java
@@ -19,8 +19,13 @@ public class ZentralesFrame extends Frame {
     addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent evt) { dispose(); }
     });
+<<<<<<< HEAD
     int frameWidth = 1071; 
     int frameHeight = 1022;
+=======
+    int frameWidth = 292; 
+    int frameHeight = 300;
+>>>>>>> 111b4f845d3d4cc3aa9528b5eaead9392d705015
     setSize(frameWidth, frameHeight);
     Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
     int x = (d.width - getSize().width) / 2;
